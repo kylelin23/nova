a4fc3a87676fbecdd99b829f49b48dce96e13b67
Kyle Lin <linkyle0924@gmail.com>
2025-05-30 18:38:27 -0700

    Add 5 year history

--- sendToRL.py
+++ sendToRL.py
@@ -6,6 +6,7 @@ def fetch_stock_market_vars():
     '''
     ticker_symbol = "UBER"
     ticker = yf.Ticker(ticker_symbol)
+    historical_data = ticker.history(period="5y")
     financials = ticker.financials
     print("\nFinancials:")
     print(financials)
